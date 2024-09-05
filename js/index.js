bb56222b150604cd601708d5539503b015ad5331
EvangelinaRodriguez <evitarodriguez40@gmail.com>
2024-09-05 09:40:27 -0300

    actualizacion de mis archivos

--- js/index.js
+++ js/index.js
@@ -1,14 +1,37 @@
-document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("autos").addEventListener("click", function() {
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("autos").addEventListener("click", function () {
         localStorage.setItem("catID", 101);
-        window.location = "products.html"
+        verifyUser()
     });
-    document.getElementById("juguetes").addEventListener("click", function() {
+    document.getElementById("juguetes").addEventListener("click", function () {
         localStorage.setItem("catID", 102);
-        window.location = "products.html"
+        verifyUser()
     });
-    document.getElementById("muebles").addEventListener("click", function() {
+    document.getElementById("muebles").addEventListener("click", function () {
         localStorage.setItem("catID", 103);
-        window.location = "products.html"
+        verifyUser()
     });
+    showUserInformation();
 });
+
+function verifyUser() {
+    // verifica si hay un usuario guardado en sessionStorage//
+    const username = sessionStorage.getItem("username");
+
+    // si no hay un usuario guardado, redirigir al formulario de inicio de sesión//
+    if (!username) {
+        window.location.href = "login.html"; // asegúrate de que este sea el nombre del archivo HTML de inicio de sesión//
+    } else {
+        window.location.href = "products.html";
+    }
+};
+
+function showUserInformation() {// se utiliza para dejar el nombre del usuario plasmado en el dropdown
+    const username = sessionStorage.getItem("username");
+
+    if (username) {
+        var userName = document.getElementById("welcome-message")
+        userName.innerHTML = username;
+    }
+};
+
